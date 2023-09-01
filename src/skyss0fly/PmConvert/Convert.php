7c14a662784e62c3cc6a5985af578e35b72dabfc
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-09-01 16:48:33 +1000

    Update Convert.php

--- src/skyss0fly/PmConvert/Convert.php
+++ src/skyss0fly/PmConvert/Convert.php
@@ -1,53 +1,65 @@
+
 <?php
 
 namespace skyss0fly\PmPluginConversion;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class Main extends PluginBase {
-public $code = $Code;
-  public function onLoad(): void {
+    public function onLoad(): void {
+        $this->getLogger()->info("Successfully Loaded PmPluginConversion. usage: /convert PluginName PMAPIVersion");
+    }
 
-    $this->getLogger("Successfully Loaded PmPluginConversion. usage: /convert PluginName PMAPIVersion");
-  }
-
-public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-    if ($command->getName() === "convert") {
-        if (count($args) < 2) {
-            $sender->sendMessage("Usage: /convert pluginname pmapiversion");
-            return false;
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "convert") {
+            if (count($args) < 2) {
+                $sender->sendMessage("Usage: /convert pluginname pmapiversion");
+                return false;
+            }
+            
+            $pluginName = $args[0];
+            $pmapiversion = $args[1];
+            
+            // Your code to Handle the conversion using $pluginName and $pmapiversion
+            $convertedCode = $this->convertPhp7ToPhp8($pluginName);
+            
+            $sender->sendMessage("Conversion completed successfully!");
+            return true;
         }
         
-        $pluginName = $args[0];
-        $pmapiversion = $args[1];
-        
-        // Your code to handle the conversion using $pluginName and $pmapiversion
-        $convertedCode = convertPhp7ToPhp8($pluginName);
-        
-        $sender->sendMessage("Conversion completed successfully!");
-        return true;
+        return false;
     }
-    
-    return false;
-}
 
-  
-function convertPhp7ToPhp8($code) {
-    // Replace deprecated functions
-    $code = str_replace("mysql_", "mysqli_", $code);
-    
-    // Replace deprecated syntax
-    $code = preg_replace("/\b(array)\s*\(/", "[$1](", $code);
-    
-    // Update error handling
-    $code = str_replace("set_error_handler", "set_exception_handler", $code);
-    
-    // Update type declarations
-    $code = preg_replace("/\b(int|float|bool|string|resource|object|array|callable|iterable)\b/", "$1", $code);
-    
-    // Update null coalescing operator
-    $code = preg_replace("/\b(\$\w+)\s*\?\?\s*(\$\w+)\b/", "isset($1) ? $1 : $2", $code);
-    
-    return $code;
+    private function convertPhp7ToPhp8($code) {
+        // Replace deprecated functions
+        $code = str_replace("mysql_", "mysqli_", $code);
+        
+        // Replace deprecated syntax
+        $code = preg_replace("/\b(array)\s*\(/", "[$1](", $code);
+        
+        // Update error handling
+        $code = str_replace("set_error_handler", "set_exception_handler", $code);
+        
+        // Update type declarations
+        $code = preg_replace("/\b(int|float|bool|string|resource|object|array|callable|iterable)\b/", "$1", $code);
+        
+        // Update null coalescing operator
+        $code = preg_replace("/\b(\?\?)\b/", "??", $code);
+
+        // Update match expression
+        $code = preg_replace("/\b(match)\b/", "match", $code);
+
+        // Update named arguments
+        $code = preg_replace("/\b(\w+)\s*=\s*(\w+)\b/", "$1: $2", $code);
+
+        // Update attribute syntax
+        $code = preg_replace("/\b(\w+)\s*@\s*(\w+)\b/", "[$2]", $code);
+
+        // Update arrow functions
+        $code = preg_replace("/\b(function)\s*\((.*?)\)\s*=>\s*(.*?)\b/", "fn($2) => $3", $code);
+
+        return $code;
+    }
 }
